bbb8e2b3cd6c82500969f0dddd7ff55f06348f9e
Kunwar Siddharth <kst85077@gmail.com>
2019-01-12 15:46:09 +0530

    project page done

--- src/components/Projectlist/Projectlist.js
+++ src/components/Projectlist/Projectlist.js
@@ -8,40 +8,71 @@ const Projectlist = () =>{
                 <hr></hr>
                 <div>eosbluff</div>
                 <div className = "proImage" id = "eosbluffImg"></div>
+                <div>link : </div>
                 <div><a href = "https://eosbluff.io" target = "_blank" rel="noopener noreferrer">https://eosbluff.io/</a></div>
-                <div>github</div>
-                <div>contribution</div>
-                <div>technologies used</div>
+                <div>contribution : </div>
+                <div className = "contribution"><p>developed smart contract(eosio blockchain)</p></div>
+                <div>technologies used : </div>
+                <div className = "tech-used">
+                    <div className = "tech">css</div>
+                    <div className = "tech">html</div>
+                    <div className = "tech">javascript</div>
+                    <div className = "tech">react</div>
+                    <div className = "tech">c++</div>        
+                </div>
             </div>
 
             <div className = "project">
                 <hr></hr>
                 <div>utopialand</div>
                 <div className = "proImage" id = "utopialandImg"></div>
+                <div>link : </div>
                 <div><a href = "https://utopialand.org" target = "_blank" rel="noopener noreferrer">https://utopialand.org/</a></div>
-                <div>github</div>
-                <div>contribution</div>
-                <div>technologies used</div>
+                <div>contribution : </div>
+                <div className = "contribution"><p>developed front end</p></div>
+                <div>technologies used : </div>
+                <div className = "tech-used">
+                    <div className = "tech">css</div>
+                    <div className = "tech">html</div>
+                    <div className = "tech">javascript</div>
+                    <div className = "tech">meteor</div>
+                    <div className = "tech">blaze template</div>
+                </div>
             </div>
 
             <div className = "project">
                 <hr></hr>
                 <div>utopia dapps</div>
                 <div className = "proImage" id = "utopiadappsImg"></div>
+                <div>link : </div>
                 <div><a href = "https://identity.utopialand.org" target = "_blank" rel="noopener noreferrer">https://identity.utopialand.org</a></div>
-                <div>github</div>
-                <div>contribution</div>
-                <div>technologies used</div>
+                <div>contribution : </div>
+                <div className = "contribution"><p>developed front end + smart contracts</p></div>
+                <div>technologies used : </div>
+                <div className = "tech-used">
+                    <div className = "tech">css</div>
+                    <div className = "tech">html</div>
+                    <div className = "tech">javascript</div>
+                    <div className = "tech">meteor</div>
+                    <div className = "tech">blaze template</div>
+                </div>
             </div>
 
             <div className = "project">
                 <hr></hr>
                 <div>this website</div>
                 <div className = "proImage" id = "thiswebsiteImg"></div>
+                <div>link : </div>
                 <div><a href = "#" target = "_blank" rel="noopener noreferrer">this website</a></div>
-                <div>github</div>
-                <div>contribution</div>
-                <div>technologies used</div>
+                <div>contribution : </div>
+                <div className = "contribution"><p>developed front end</p></div>
+                <div>technologies used : </div>
+                <div className = "tech-used">
+                    <div className = "tech">css</div>
+                    <div className = "tech">html</div>
+                    <div className = "tech">javascript</div>
+                    <div className = "tech">react</div>
+                </div>
             </div>
         </div>
     )
